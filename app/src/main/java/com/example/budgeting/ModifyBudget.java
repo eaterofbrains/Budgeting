e7b6689ef92dbc7f741685679dce01027cc7e3e4
vihangap98 <68697868+vihangap98@users.noreply.github.com>
2020-10-04 22:22:49 +0530

    List budget added (requires removal of main xml and activity_list_budgets.xml)

--- app/src/main/java/com/example/budgeting/ModifyBudget.java
+++ app/src/main/java/com/example/budgeting/ModifyBudget.java
@@ -5,23 +5,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class ModifyBudget extends AppCompatActivity {
     //IT19097084, Ayodya Hettiarachchi
+
+    //declare elements used
+    private TextInputEditText changeBudgetName, changeBudgetAmount;
+    private Button applyChanges, deleteBudget;
+    private TextView currentName, currentTarget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_budget);
+
+        //find declared elements
+        //TextInputEditTexts
+        changeBudgetName = findViewById(R.id.changeBudgetName);
+        changeBudgetAmount = findViewById(R.id.changeBudgetAmount);
+        //Buttons
+        applyChanges = findViewById(R.id.applyChanges);
+        deleteBudget = findViewById(R.id.deleteBudget);
+        //TextView
+        currentName = findViewById(R.id.currentName);
+        currentTarget = findViewById(R.id.currentTarget);
+
+        //call functions when button is tapped
+        applyChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoViewExistingBudget();
+            }
+        });
+        deleteBudget.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                gotoListBudgets();
+            }
+        });
     }
 
-    public void gotoViewExistingBudget(View next){
-        Intent i = new Intent (this, ViewExistingBudget.class);
+    //function enabling navigation to view existing budgets
+    public void gotoViewExistingBudget(){
+        Intent i = new Intent(this, ViewExistingBudget.class);
         startActivity(i);
     }
 
-    public void gotoBudgetList(View next){
-        Intent i = new Intent (this, MainActivity.class);
+    //function enabling navigation to main list of budgets when deleted
+    public void gotoListBudgets() {
+        Intent i = new Intent(this, listBudgets.class);
         startActivity(i);
     }
 }
